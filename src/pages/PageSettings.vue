62cf932d4c667644636afdc7200ecf0ed45c9240
Nikolina <71445284+nikolina2k@users.noreply.github.com>
2023-04-18 01:21:49 +0300

    create left navbar, navigation links and two pages

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,13 @@
+<template>
+    <q-page padding>
+      <p>settings page</p>
+    </q-page>
+  </template>
+  
+  <script>
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({});
+  </script>
+  
+  <style></style>
